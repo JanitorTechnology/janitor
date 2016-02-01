a692a7ce83599653cbdf5972053db95281a3589b
Jan Keromnes <janx@linux.com>
2016-02-01 12:31:24 +0100

    Add machine status badges.

--- static/js/projects.js
+++ static/js/projects.js
@@ -17,3 +17,31 @@ Array.map(document.querySelectorAll('a[data-action="spawn"]'), function (link) {
   }));
 
 });
+
+
+// Add status badges to elements with a 'data-status' attribute.
+
+Array.map(document.querySelectorAll('*[data-status]'), function (element) {
+
+  var span = document.createElement('span');
+  var status = element.dataset.status;
+
+  // Format the status.
+  span.textContent = status.split('-').join(' ');
+  span.classList.add('text-capitalize');
+
+  // Choose a relevant bootstrap label class.
+  var label = {
+    started: 'success',
+    accepted: 'success',
+    rejected: 'warning',
+    merged: 'primary'
+  };
+  label['build-failed'] = 'danger';
+  label['start-failed'] = 'danger';
+  label['update-failed'] = 'warning';
+  span.classList.add('label', 'label-' + (label[status] || 'default'));
+
+  element.appendChild(span);
+
+});
