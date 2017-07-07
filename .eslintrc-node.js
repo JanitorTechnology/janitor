b118bb5d91f757413abad7b054ee80925e796c08
Mathieu Agopian <mathieu@agopian.info>
2017-07-07 14:39:30 +0000

    Simplify/reduce the eslintrc files (copy/pasting is bad)

--- .eslintrc-node.js
+++ .eslintrc-node.js
@@ -1,6 +1,6 @@
 module.exports = {
   "env": {
-    "browser": true,
+    "browser": false,
     "es6": true,
     "node": true
   },
@@ -9,19 +9,11 @@ module.exports = {
     "plugin:node/recommended",
     "standard"
   ],
-  "parserOptions": {
-    "ecmaVersion": "2017",
-    "ecmaFeatures": {
-      "experimentalObjectRestSpread": true,
-      "jsx": true
-    },
-    "sourceType": "module"
-  },
   "plugins": [
     "node"
   ],
   "rules": {
-    // Standard js rules
+    // Override some of standard js rules
     "semi": ["error", "always"],
     "comma-dangle": [
       "error", {
@@ -32,47 +24,5 @@ module.exports = {
         "functions": "never",
       }
     ],
-    
-    // Node require rules
-    "node/exports-style": ["error", "module.exports"],
-
-    // overriding recommended rules
-    "no-constant-condition": ["error", { checkLoops: false }],
-    "no-console": [ "error", { allow: ["log", "warn", "error"] } ],
-    "no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
-
-    // possible errors
-    "array-callback-return": "error",
-    "consistent-return": "error",
-    "default-case": "error",
-    "dot-notation": "error",
-    "eqeqeq": "error",
-    "for-direction": "error",
-    "no-alert": "error",
-    "no-caller": "error",
-    "no-eval": "error",
-    "no-extend-native": "error",
-    "no-extra-bind": "error",
-    "no-extra-label": "error",
-    "no-implied-eval": "error",
-    "no-invalid-this": "error",
-    "no-return-await": "error",
-    "no-self-compare": "error",
-    "no-throw-literal": "error",
-    "no-unmodified-loop-condition": "error",
-    "no-unused-expressions": "error",
-    "no-useless-call": "error",
-    "no-useless-computed-key": "error",
-    "no-useless-concat": "error",
-    "no-useless-constructor": "error",
-    "no-useless-rename": "error",
-    "no-useless-return": "error",
-    "no-var": "error",
-    "no-void": "error",
-    "no-with": "error",
-    "prefer-const": "error",
-    "prefer-promise-reject-errors": "error",
-    "prefer-rest-params": "error",
-    "prefer-spread": "error",
   }
 };
