9e3d2cbca027ef3275c3e6b4d6f965170e4289e7
Arshad Khan <arshdkhn1@gmail.com>
2017-09-02 20:12:54 +0530

    new design

--- static/js/janitor.js
+++ static/js/janitor.js
@@ -293,3 +293,10 @@ if ('serviceWorker' in navigator) {
       console.error(error);
     });
 }
+
+function toggleMenu() {
+  document.querySelector('.menu-toggle').classList.toggle('on')
+  document.querySelector('.navbar-menu').classList.toggle('open');
+}
+
+document.querySelector('.menu-toggle').addEventListener("click", toggleMenu);
