e0987057470d36c2b74c6d64e8724f5df683e205
Jan Keromnes <janx@linux.com>
2018-01-25 10:20:56 +0000

    Remove unused c9.io usernames from server and web app.

--- static/js/settings.js
+++ static/js/settings.js
@@ -1,19 +1,6 @@
 // Copyright © 2015 Jan Keromnes. All rights reserved.
 // The following code is covered by the AGPL-3.0 license.
 
-// Settings: Cloud9 username form.
-
-ajaxForm('#cloud9user-form', 'key', function (form, data) {
-  var status = 'error';
-  var message = data.message;
-
-  if (data.status === 'key-saved') {
-    status = 'success';
-  }
-
-  updateFormStatus(form, status, message);
-});
-
 // Settings: Cloud9 SSH key form.
 
 ajaxForm('#cloud9-form', 'key', function (form, data) {
