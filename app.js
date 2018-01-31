db0bbd4f4bb6aa799437fede8c697ce704b02483
Arshad Khan <arshdkhn1@gmail.com>
2018-02-01 05:20:53 +0530

    Addressing janx's nits on the new 404 page.

--- app.js
+++ app.js
@@ -447,11 +447,11 @@ boot.executeInParallel([
       });
     });
   });
-  
+
   // New 404 Not Found page
-  app.route(/^\/404-new$/, (data, match, end, query) => {
+  app.route(/^\/404-new\/?$/, (data, match, end, query) => {
     const { user } = query.req;
-    log('404', match[0]);
+    log('404-new', match[0]);
     routes.notFoundPageNew(query.res, user);
   });
 
